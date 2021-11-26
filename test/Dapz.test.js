f0df79958a78197ea2f15afd4c0b65767046e16e
krishna sivapalan <krish.96@hotmail.com>
2021-11-26 22:47:22 +0000

    -first basic passing test

--- test/Dapz.test.js
+++ test/Dapz.test.js
@@ -2,5 +2,15 @@ const Dapz = artifacts.require("Dapz")
 
 contract("Dapz", (accounts) => {
 
-    
+    before(async () => {
+        dapz = await Dapz.deployed()
+    })
+
+    it("starts 1M", async () =>{
+        let balance = await dapz.balanceOf(accounts[0])
+        console.log(web3.utils.fromWei(balance), 'ether')
+    })
+
+
+
 })
