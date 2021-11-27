357723222b3ef96ee061cd2f79362bafaebc6d0d
krishna sivapalan <krish.96@hotmail.com>
2021-11-27 12:30:21 +0000

    -basic game functionality coded into Dapz.sol with mint reward and 1 day timeout/reset period

--- contracts/Dapz.sol
+++ contracts/Dapz.sol
@@ -14,7 +14,8 @@ contract Dapz is ERC20 {
     mapping(address => mapping(address=>uint)) public dailyDaps;
     mapping(address => mapping(address=>uint)) public lastDap; 
     mapping(address => mapping(address=>bool)) public timedOut;
-    mapping(address => mapping(address=>bool)) public doubleDown
+    mapping(address => mapping(address=>bool)) public doubleDown;
+    mapping(address => mapping(address=>bool)) public claimable;
 
 
     constructor() public ERC20("Dapz", "DAPZ") {
@@ -29,42 +30,84 @@ contract Dapz is ERC20 {
 
     }
 
-    function _dapRoll(address friend) internal returns(uint) {
-        return uint(keccak256(abi.encodePacked(msg.sender,friend,now))) % (10**10);
+    function _dapRoll(address sender, address friend) internal returns() {
+        lastDap[sender][friend] = now;
+        timedOut[sender][friend] = true;
+        dailyDaps[sender][friend] = uint(keccak256(abi.encodePacked(sender,friend,now))) % (10**10);
+
     }
 
-    function firstDailyRoll(address friend) public returns(){
-        
-        if( (timedOut[msg.sender][friend] == false) || (timedOut[friend][msg.sender] == false)){
-            dailyDaps[msg.sender][friend] = _dapRoll(friend);
-            lastDap[msg.sender][friend] = now;
-            timedOut[msg.sender][friend] = true;
+    function _checkChallengeAndMint(address sender, address friend) internal returns(){
+        bool senderCheck = dailyDaps[sender][friend] < challenge;
+        bool friendCheck = dailDaps[friend][sender] < challenge;
 
+        if (senderCheck && friendCheck){
+            _mint(sender, 3*dailyReward);
+            _mint(friend, 3*dailyReward);
         }
-        else if(timedOut[msg.sender][friend] == true){
+        else if (senderCheck){
+            _mint(sender, dailyReward);
+            _mint(friend, uint(dailyReward/2));
+        }
+        else if (friendCheck){
+            _mint(sender, uint(dailyReward/2));
+            _mint(friend, dailyReward);
+        }
 
-            if( ((lastDap[msg.sender][friend]-now) > (1 days + 2000)) ){
+    }
 
-                if( (((lastDap[friend][msg.sender]-now) > (1 days + 2000)) && timedOut[friend][msg.sender]==true) || (timedOut[friend][msg.sender] == false)){
-                    
-                    lastDap[msg.sender][friend] = now;
-                    timedOut[msg.sender][friend] = true;
-                    timedOut[friend][msg.sender] = false;
-                    
-                    
-                }
-                else {
-                    revert();
-                }
+    function _dayPassed(address first, address second) internal returns(bool){
+        return (lastDap[first][second]-now) > (1 days + 2000);
+
+    }
+
+    function _resetTimer(address sender, address friend) internal returns(){
+        timedOut[sender][friend] = false;
+        timedOut[friend][sender] = false;
+    }
+
+    
+
+    function firstDailyRoll(address friend) public returns(){
+
+        
+        if(!(timedOut[msg.sender][friend] && timedOut[friend][msg.sender])){
+            _dapRoll(msg.sender, friend);
+        }
+
+        else if (timedOut[msg.sender][friend] && timedOut[friend][msg.sender]){
+            if (_dayPassed(msg.sender, friend) && _dayPassed(friend, msg.sender)){  
+                _resetTimer(msg.sender, friend);
+                _dapRoll(msg.sender, friend);
+
+                
             }
             else {
                 revert();
             }
         }
-        else if((timedOut[friend][msg.sender]==true) && timedOut[msg.sender][friend]==false){
+
+        else if(timedOut[msg.sender][friend]){
+
+            if( (_dayPassed(msg.sender, friend)){
+                    
+                _dapRoll(msg.sender, friend);                
+            }
+            else {
+                revert();
+            }
+        }
+        else if((timedOut[friend][msg.sender])){
             // 1st partner has already rolled, call a function that lets 2nd
             // partner roll and double down with respect to 1st roll
+            _dapRoll(msg.sender, friend);
+
+            _checkChallengeAndMint(msg.sender, friend);
+
+
         }
+
+
 
 
 
